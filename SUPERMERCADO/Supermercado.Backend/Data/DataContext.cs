4ba4ed7391f286406b8830efbd897c6a8eb38ee1
Kevin Montaño Restrepo <kevinmontano1121108@correo.itm.edu.co>
2025-09-13 20:48:01 -0500

    add rol entitie

---
+++ SUPERMERCADO/Supermercado.Backend/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Supermercado.Shared.Entities;
+
+namespace Supermercado.Backend.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+    public DbSet<Rol> Rols { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Rol>().HasIndex(x => x.nombre).IsUnique();
+    }
+}
