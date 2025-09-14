36c7115ad5a78d943466ef32080c4def1a2fcb22
Kevin Montaño Restrepo <kevinmontano1121108@correo.itm.edu.co>
2025-09-14 10:34:48 -0500

    creacion de controlador get y post

---
+++ SUPERMERCADO/Supermercado.Backend/Controllers/RolsController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Supermercado.Backend.Data;
+using Supermercado.Shared.Entities;
+
+namespace Supermercado.Backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RolsController : ControllerBase
+{
+    private readonly DataContext _context;
+    public RolsController(DataContext context)
+    {
+            _context = context; 
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAsync()
+    {
+        try
+        {
+            return Ok(await _context.Rols.ToListAsync());
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> postAsync(Rol rol)
+    { 
+        try
+        {
+            _context.Rols.Add(rol);
+            await _context.SaveChangesAsync();
+            return Ok(rol);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+    }
+
+    [HttpPut("{id}")]
+
+    public async Task<IActionResult> PutAsync(int id, Rol rol)
+    {
+        if (id != rol.rol_id)
+            return BadRequest("El id del rol no coincide con el id");
+        var existingRol = await _context.Rols.FindAsync(id);
+        if (existingRol == null)
+            return NotFound("El rol no existe");
+
+        existingRol.nombre = rol.nombre;
+        try
+        {
+            await _context.SaveChangesAsync();
+            return Ok(existingRol);
+        }
+        catch (Exception ex)
+        {
+
+            return StatusCode(500, ex.Message);
+        }
+    }
+
+}
--- SUPERMERCADO/Supermercado.Backend/Program.cs
+++ SUPERMERCADO/Supermercado.Backend/Program.cs
@@ -6,7 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+
 
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -16,12 +17,13 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
 
